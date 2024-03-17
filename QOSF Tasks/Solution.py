1c69fdf0b4575acba8702f00dd62edbd28d058f5
Jordan Wiseman <74243511+JWiseman-git@users.noreply.github.com>
2024-03-17 15:37:21 +0000

    Learning Basics

--- QOSF Tasks/Solution.py
+++ QOSF Tasks/Solution.py
@@ -1,4 +1,16 @@
 import cirq
 
-q = cirq.lineQubits(1)
+q = cirq.LineQubit(1)
 
+I = cirq.unitary(cirq.X)
+
+o = cirq.H(cirq.LineQubit(1))
+
+circuit = cirq.Circuit()
+qubits = cirq.LineQubit.range(3)
+circuit.append(cirq.H(qubits[0]))
+circuit.append(cirq.H(qubits[1]))
+circuit.append(cirq.H(qubits[2]))
+print(circuit)
+
+print(cirq.Circuit(cirq.SWAP(q, q + 1) for q in cirq.LineQubit.range(3)))
